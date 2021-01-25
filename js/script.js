de7b67160dd6d849c11b0c5ad0bbeaeda5678a5a
Shilldon <stewart.s-hill@ntlworld.com>
2021-01-25 22:45:52 +0000

    updated code comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-
 $(window).on('load',function(){
   $('#instructionsModal').modal('show');
 });
 $(document).ready(function () {  
   outside = true;
+  /*Each chest of decorations is dynamically populated on page load*/
   populateIconTable();
-
   
+  /*Ensure that the snowfall gif has been hidden on page load so it can be turned on
+  by the snow button*/
   $(".snow").css("background-image", "none");
+
+  /*Ensure the carollers mouths are return to the starting background image position*/
   mouth("REST");
 
+  /*Apply the slider parameters to the slide switch to enable the overlay opacity to be adjusted
+  correctly*/
   $(function() {
     $("#slider").slider({
       step: 0.1,
@@ -31,38 +36,39 @@ $(document).ready(function () {
      
     });
 
+    /*droppable event added to "snow" div to enable baubles dragged off the tree to be removed*/
     $('.snow').on('drop',  function (event, ui) {
+      /*outside variable is set if a bauble is dragged outside of the tree area and reset if
+      the bauble is dragged back in. 
+      If a bauble is dropped outside of the tree area it is hidden*/
       if (outside) {
-        var newPositionLeft = $("#baubles-collection").position().left - ui.helper.position().left;
-        var newPositionTop = $("#baubles-collection").position().top - ui.helper.position().top;
-        if(ui.helper) {
-          //ui.helper.css("transition-duration", "0.5s");
-          //ui.helper.css({ "-webkit-transform": "translate(" + newPositionLeft + "px," + newPositionTop + "px)" });
+         if(ui.helper) {
           setTimeout(function () { ui.helper.css("display", "none");}, 500);
           ui.helper.detach();
         }
       }
     })
 
+    /*initial set up for all icons with draggable icon class*/
     $(".draggable-icon").draggable({
       containment: "window",
       revert: 'invalid',
       helper: 'clone',
     });
 
+    /*created icon class is applied on a bauble being removed from the chest.
+    This is required because of the need to clone draggable icons*/
     $(".created-icon").draggable({
       containment: "window",
     })
 
+    /*event triggered by draggable-icon or created-icon being dropped in a droppable area/
+    On dropping the icon a clone is created and attached to the droppable area. The ui-helper is removed*/
     $(".droppable").droppable({
       accept: ".draggable-icon, .created-icon",
       drop: function (event, ui) {
         var uiLeft = ui.offset.left;
         var uiTop = ui.offset.top;
-        var treeLeft = $(this)[0].offsetLeft;
-        var treeTop = $(this)[0].offsetTop;
-        var moveUiLeft = uiLeft - treeLeft;
-        var moveUiTop = uiTop - treeTop;
         if(ui.helper.hasClass("draggable-icon")) {
           ui.helper.removeClass("draggable-icon");
           ui.helper.addClass("created-icon");
@@ -84,62 +90,73 @@ $(document).ready(function () {
       }
     });
 
+    /*To enable reposition of dropped decorations double clicking highlights the decoration
+    with dropshadow and icon is detached from the dropped area*/
     $(document).on("dblclick", ".dropped", function (event) {
       $(".decoration-icon").each(function () {
         $(this).css("filter", "");
-      })
-      
+      });
       $(this).css("filter", "drop-shadow(2px 0px 0px white) drop-shadow(-2px 0px 0px white) drop-shadow(0px 2px 0px white) drop-shadow(0px -2px 0px white)");
       var uiLeft = $(this)[0].offsetLeft;
       var uiTop = $(this)[0].offsetTop;
-      var treeLeft = $(".tree-container")[0].offsetLeft;
-      var treeTop = $(".tree-container")[0].offsetTop;
-      var moveUiLeft = uiLeft + treeLeft;
-      var moveUiTop = uiTop + treeTop;
-      //$(this).css("filter", "drop-shadow(0px 0px 25px white)");
       $(this).detach().css({ "left": uiLeft, "top": uiTop }).appendTo("body");
       $(this).removeClass("dropped");
       $(this).focus();
       $(this).draggable();
+    });
 
-    })
-
+    /*Switch events to control on off status of buttons and to call associated functions*/
     $(".switch").click(function () {
+      //restart switch to reload the page (no on off status)
       if($(this).hasClass("restart-switch")) {
         restart();
       }     
+      //all other buttons have on off status
+      //status set to off
       else if ($(this).attr("on") == 1) {
+        /*NB play switch (that shows the carollers) off status is set during animation
+        This is to prevent the play switch being set to off halfway through the animation 
+        causing issues on restarting the animation at that point.*/
         if(!$(this).hasClass("play-switch")) { 
           $(this).attr("on", 0);
+          //switch to control lighting effects
           if ($(this).hasClass("light-switch")) {
             $(this).find("img").attr("src", "graphics/controls/switch-off.png");
             switchOffLights(); 
           }
+          //switch to control snow animation
           else if ($(this).hasClass("snow-switch")) {
             $(".snow").css("background-image", "none");
             $(this).css("filter", "none");
           } 
+          //switch to show and hide decorations menu
           else if ($(this).hasClass("ornaments-menu")) {
             hideDecorationsMenu();
           }       
         }  
       }
       else {
+        //status set to on and call relevant functions
         $(this).attr("on", 1);
+        //switch to control lighting effects
         if ($(this).hasClass("light-switch")) {
           $(this).find("img").attr("src", "graphics/controls/switch-on.png");
           switchOnLights();
         }
+         //switch to control snow animation
         else if ($(this).hasClass("snow-switch")) {
           $(".snow").css("background-image", "");
           $(this).css("filter", "drop-shadow(2px 0px 0px white) drop-shadow(-2px 0px 0px white) drop-shadow(0px 2px 0px white) drop-shadow(0px -2px 0px white)");
         }
+        //Switch to show the carollers
         else if($(this).hasClass("play-switch")) {
           startCarolling();
-        }     
+        }  
+        //switch to show and hide decorations menu   
         else if ($(this).hasClass("ornaments-menu")) {
           showDecorationsMenu();
         }     
+        //switch to hide the carollers
         else if($(this).hasClass("cancel-carollers")) {
           hideCarollers();
           $(this).css("display","none");
@@ -148,7 +165,7 @@ $(document).ready(function () {
     });
   });
 
-
+  //Chest control button - to play the open or close animation
   $(".chest-button").click(function () {
     if ($(".chest").attr("chest-open") == 1) {
       $(".chest").addClass("chest-closed");
@@ -163,10 +180,14 @@ $(document).ready(function () {
     }
   })
 
+  /*Each decorations tab click will hide any chest already shown
+  and show its associated decorations chest*/
   $(".tab-in").click(function () {
     var chestName = $(this).attr("chest");
     var $chest = $("." + chestName + "-chest");
+    /*once a decorations chest has been selected - hide the decorations menu*/
     hideDecorationsMenu();
+    /*if the selected decorations chest is already showing then hide it*/
     if ($(this).attr("showing") == 1) {
       $chest.removeClass("show-chest");      
       $chest.addClass("hide-chest");
@@ -178,6 +199,7 @@ $(document).ready(function () {
         $chest.css("transition-duration", "");
       }, 1000);
     }
+    /*otherwise hide any other chests that are showing and show the selected chest*/
     else {
       $(".chest").each(function () {
         if (!$(this).hasClass(chestName + "-chest")) {
@@ -200,8 +222,10 @@ $(document).ready(function () {
     }
   })
 
+  /*On a chest being displayed and opened the list of decorations will slide up
+  out of the chest. A delay is required before displaying the list of decorations
+  to enable time for the chest opening animation to complete*/
   $('[data-delayed-toggle="collapse"]').on('click', function (e) {
-
     var delay = $(this).data('delay') || 500;
     var $target = $($(this).attr("href"));
     if ($target.hasClass('show')) {
@@ -215,6 +239,7 @@ $(document).ready(function () {
 
 })
 
+/*Function to dynamically populate each of the decorations boxes with the appropriate images*/
 function populateIconTable() {
   var decorations = { "baubles": 13, "lights": 16, "stars": 4, "candy-canes": 6, "ornaments": 4, "presents": 10 };
   $.each(decorations, function (key, value) {
@@ -235,6 +260,11 @@ function populateIconTable() {
       if (i == 1) {
         tableContent = tableContent + "<tr>";
       }
+      /*If the decoration item requires an animation add the relevant extra class
+      Lights - animation adjusts opacity and applies a drop shadow to simulate flashing
+      Stars - animation adjusts a drop shadow to simulate twinkling 
+      Most decorations are static png files some are animated gifs - appropriate
+      file suffix is applied to ensure correct image is loaded to page*/
       if (key == "lights") {
         additionalClass = "light";
       }
@@ -252,6 +282,7 @@ function populateIconTable() {
         additionalClass = "candle";
       }
       var decorationClass = key.slice(0,-1);
+      /*Build the contents of the chest by adding HTML code*/
       tableContent = tableContent + `<td><img class="draggable-icon created-icon ui-widget-content decoration-container decoration ${decorationClass} ${additionalClass}" src='graphics/${key}/${key.slice(0, -1)}-${i}.${gfx}'></td>`;
       if (i % tableDecorationWidth === 0) {
         tableContent = tableContent + "</tr>";
@@ -263,9 +294,15 @@ function populateIconTable() {
   })
 }
 
+/*Function to animate christmas lights*/
 function switchOnLights() {
   var time = 0;
   $(".light").each(function () {
+    /*check to see if the light has been dropped on the tree and if so, only then apply the addLight css
+    animation.
+    A delay is set to each the class being added to each light to ensure that the lights flash in 
+    a sequence, otherwise all lights will flash on and off at the same time.
+    The check for the dropped class ensure that lights  'in the decoration box' are not flashing.*/
     if ($(this).hasClass("dropped")) {
       time += 150;
       if (time > 1000) {
@@ -276,6 +313,9 @@ function switchOnLights() {
     }
   });
 
+  /*If a star decoration has been added to the tree and has the dropped class apply the 
+  css addStarLight animation to simulate twinkling.
+  The check for the dropped class ensure that stars 'in the decoration box' are not twinkling.*/
   $(".star").each(function () {
     if ($(this).hasClass("dropped")) {
       $(this).addClass("addStarlight");
@@ -283,6 +323,7 @@ function switchOnLights() {
   })
 }
 
+/*Function to remove opacity and drop shadow css animations from lights and stars.*/
 function switchOffLights() {
   $(".star").each(function () {
     var time = 0;
@@ -339,41 +380,17 @@ function restartAnimation(elem) {
   }, 20);
 }
 
-function setupDecorations(id) {
-  var treeInfo = localStorage.getItem("treePosition-"+id);
-  var treePosition = JSON.parse(treeInfo);
-  if(treeInfo!=null) {
-  $(".draggable-tree").css({"position":"absolute", "top":treePosition.top, "left":treePosition.left});
-  var setupDecorations = localStorage.getItem("decorations-"+id);
-  var decorationsInfo = JSON.parse(setupDecorations);  
-  for(i=0;i<=decorationsInfo.length-1;i++) {
-    decoration=$('<img />', { 
-      src:decorationsInfo[i]["source"]
-    }).appendTo($("body"))
-    decoration.addClass("created-icon decoration decoration-container dropped")
-    if(decorationsInfo[i]["star"]==true) {
-      decoration.addClass("star");      
-    }
-    if(decorationsInfo[i]["light"]==true) {
-      decoration.addClass("light");      
-    }    
-    if(decorationsInfo[i]["candle"]==true) {
-      decoration.addClass("candle");      
-    }        
-    decoration.css("position","absolute")
-    decoration.detach().css({ "left": decorationsInfo[i]["position-x"], "top": decorationsInfo[i]["position-y"] }).appendTo(".tree-container");
-  }
-}
-}
-
 function restart() {
   location.reload();
 }
 
+/*function to animate the carollers mouths operated by timed function calls*/
 function mouth(letter) {
   var xMouth;
   var yMouth;
-
+  /*The mouth image is a background image containing 12 images of mouth positions. 
+  By adjusting the background position by specific amounts will show the relevant mouth
+  position associated with the appropriate phonic.*/
   switch(letter) {
     case "A": xMouth = 0; yMouth = 0;
     break;
@@ -401,6 +418,7 @@ function mouth(letter) {
     break;         
  
   }
+  /*REST argument ensures the mouth will be hidden before and after animation*/
   if(letter=="REST") {
     $(".mouth").hide();
   }
@@ -410,20 +428,22 @@ function mouth(letter) {
   }
 }
 
+/*function to animate carollers onto the screen when the play button is pressed*/
 function showCaroller(caroller,i) {
-  
   setTimeout(function() {
     caroller.css("opacity","1");
     caroller.addClass("bounce-in");
   }, i*50);
 }
 
+/*function to remove carollers from the screen when the reset button is pressed*/
 function hideCarollers() {
   $("audio").trigger("stop");
   $(".caroller").removeClass("bounce-in");
   $(".caroller").css("opacity",0);
 }
 
+/*function to set up and play the caroller animation*/
 function startCarolling() {
   //hide the menus
   $("#menucollapse").collapse('hide');
@@ -437,58 +457,57 @@ function startCarolling() {
     $(".light-switch").attr("on",1);
   }  
 
-//hide any decoration chests that are out
-//close any chests that are showing
-$(".chest").each(function () {
-  if ($(this).attr("chest-open") == 1) {
-    $(this).addClass("chest-closed");
-    $(this).removeClass("chest-open");
-    restartAnimation($('.chest-closed'))
-    $(this).attr("chest-open", 0);
-    var $target = $($(this).find("button").attr("href"));
-    if ($target.hasClass('show')) {
-      $target.collapse('hide');
-    }
-  }
-})
-
-//check if any of the tabs are marked as showing a chest, if so, reset and hide chest
-//with a delay to allow time for the chest to close
-//then add the 'remove carollers' option to the menu
-setTimeout(function() {
-  $(".tab-in").each(function() { 
-    var chestName = $(this).attr("chest");
-    var $chest = $("." + chestName + "-chest");
-    if ($(this).attr("showing") == 1) {
-      $chest.removeClass("show-chest");      
-      $chest.addClass("hide-chest");     
-      console.log("hiding chest") 
-      //$chest.css("transform", "translate(-25vw,0)");
-      $(this).attr("showing", 0);
-      $chest.css("transition", "ease-in-out");
-      $chest.css("transition-duration", "1s");
-      setTimeout(function () {
-        $chest.css("transition", "");
-        $chest.css("transition-duration", "");
-      }, 1000);
-    }{
-
+  //hide any decoration chests that are out
+  //close any chests that are showing
+  $(".chest").each(function () {
+    if ($(this).attr("chest-open") == 1) {
+      $(this).addClass("chest-closed");
+      $(this).removeClass("chest-open");
+      restartAnimation($('.chest-closed'))
+      $(this).attr("chest-open", 0);
+      var $target = $($(this).find("button").attr("href"));
+      if ($target.hasClass('show')) {
+        $target.collapse('hide');
+      }
     }
   });
-  $(".cancel-carollers").css("display","inline-block");
-},1000)
 
+  //check if any of the tabs are marked as showing a chest, if so, reset and hide chest
+  //with a delay to allow time for the chest to close
+  //then add the 'remove carollers' option to the menu
+  setTimeout(function() {
+    $(".tab-in").each(function() { 
+      var chestName = $(this).attr("chest");
+      var $chest = $("." + chestName + "-chest");
+      if ($(this).attr("showing") == 1) {
+        $chest.removeClass("show-chest");      
+        $chest.addClass("hide-chest");     
+        $(this).attr("showing", 0);
+        $chest.css("transition", "ease-in-out");
+        $chest.css("transition-duration", "1s");
+        setTimeout(function () {
+          $chest.css("transition", "");
+          $chest.css("transition-duration", "");
+        }, 1000);
+      }{
 
-
+      }
+    });
+    /*show the menu button to enable the carollers to be removed from the scene*/
+    $(".cancel-carollers").css("display","inline-block");
+  },1000);
 
   
+  /*Ensure all the carollers are hidden from view before starting the animation to show them*/
   var i=0;
   hideCarollers();
   $(".caroller").each(function() {
     i++;
     showCaroller($(this),i);
-  })
+  });
 
+  /*play the audio then call function to adjust mouth position at relevant timed points during 
+  audio*/
   $("audio").trigger("play");
   time = 11800;
   setTimeout(function() { mouth("E")},time); //WE
@@ -603,6 +622,9 @@ setTimeout(function() {
   time+=1000;
   setTimeout(function() { mouth("REST")},time); //REST 
   time+=500;      
+
+  /*At end of carol display merry Christmas sign and revert the caroller play switch to off
+  so the animation can be played again by pressin gthe button*/
   setTimeout(function() { 
     $(".play-switch").attr("on", 0);
     $(".christmas-sign").fadeIn(0);
@@ -613,8 +635,7 @@ setTimeout(function() {
     $(".christmas-text").attr("src","./graphics/backgrounds/christmas-text.gif");
   }, time);
 
-
-
+  /*Add snow, light and nighttime effects (if not already on) and play gentle jingle bells*/
   time+=5000;
   setTimeout(function() {
     $("audio").attr("src","./sounds/jinglebells.wav"); 
@@ -639,9 +660,11 @@ setTimeout(function() {
 
 }
 
+/*On click button to show decorations - function to slide deoration options from top of page*/
 function showDecorationsMenu() {
   $(".ornaments-menu").find("img").attr("src","./graphics/controls/ornaments-menu-open.png");
   $(".tab-in").addClass("slide-down");
+  /*if any chests with decorations are currently show, animate close and hide the chest*/
   $(".chest").each(function () {
     if ($(this).attr("chest-open") == 1) {
       $(this).addClass("chest-closed");
@@ -656,6 +679,7 @@ function showDecorationsMenu() {
   })
 }
 
+/*On click of the decorations menu for a second time, slide the decorations menu back up above sreen*/
 function hideDecorationsMenu() {
   $(".ornaments-menu").attr("on", 0);
   $(".ornaments-menu").find("img").attr("src","./graphics/controls/ornaments-menu.png");
